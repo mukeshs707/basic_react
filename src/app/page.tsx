9061ce01b742431400303284bc91e0b5e527e401
Mukesh Singh <mukeshsingh@codebrewinnovations.com>
2025-05-13 18:52:09 +0530

    text changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="text-4xl font-bold mb-4">Welcome to Next.js</h1>
-        <p>Get started by editing src/app/page.tsx</p>
+        <h1 className="text-4xl font-bold mb-4">Welcome to BlockTech Brew</h1>
+        <p>Chandigarh, India</p>
       </div>
     </main>
   )
